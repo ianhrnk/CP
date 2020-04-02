a4e608ef8c73b5038be9840ef5b16e2cbf089446
ianhrnk <ian.hrnk@gmail.com>
2020-04-01 20:36:06 -0400

    add 1030A

--- codeforces/1030A.cpp
+++ codeforces/1030A.cpp
@@ -2,6 +2,19 @@
 using namespace std;
 
 int main() {
-
-
+    int n, j;
+    bool k = false;
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        cin >> j;
+        if (j == 1) {
+            k = true;
+            break;
+        }
+    }
+    if (k)
+        cout << "HARD" << endl;
+    else
+        cout << "EASY" << endl;   
+    return 0;
 }
