2721c411afc21d186ad3d97b9341f4d9908908dd
Ian H <ian.hrnk@gmail.com>
2020-08-24 16:09:13 -0400

    680B

--- codeforces/B/bear_and_finding_criminals.cpp
+++ codeforces/B/bear_and_finding_criminals.cpp
@@ -3,8 +3,33 @@ using namespace std;
 
 int main()
 {
-  
-  
+  int n, a, criminals = 0;
+  cin >> n >> a;
+  int arr[n];
 
+  for (int &i : arr)
+    cin >> i;
+  a--;
+  if (arr[a] == 1)
+    criminals++;
+  for (int i = 1; a+i < n || a-i >= 0; ++i)
+  {
+    if (a+i >= n && a-i >= 0)
+    {
+      if (arr[a-i] == 1)
+        criminals++;
+    }
+    else if (a+i < n && a-i < 0)
+    {
+      if (arr[a+i] == 1)
+        criminals++;
+    }
+    else if (arr[a+i] == 1 || arr[a-i] == 1)
+    {
+      if (arr[a+i] == 1 && arr[a-i] == 1)
+        criminals += 2;
+    }
+  }
+  cout << criminals << endl;
   return 0;
 }
